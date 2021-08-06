ee05d97fb67b0998410c1872ad7305b3c2632f80
inabororevaoghene@gmail.com <inabororevaoghene@gmail.com>
2021-08-06 10:37:04 +0100

    week 10 day 1

---
+++ script.js
@@ -0,0 +1,17 @@
+let user = {
+    name: 'James',
+    age: 22
+};
+let userParagraph = document.getElementById('user');
+
+function signIn() {
+    window.localStorage.setItem('user', JSON.stringify(user));
+}
+function getUser(){
+    const user = JSON.parse(window.localStorage.getItem('user'));
+    userParagraph.innerHTML = `${user.name} ${user.age}`
+}
+function logOut(){
+    localStorage.removeItem('user')
+    userParagraph.innerHTML='';
+}
